24cf42ccf37a9ddd39be20907fd4807e77e9bf01
Stephen <stephen.swaringin@gmail.com>
2023-12-24 05:57:54 -0600

    Added testing for empty stings

--- bundle.js
+++ bundle.js
@@ -1,49 +1,60 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-let Phrase = require("mhartl-palindrome");
+let Phrase = require("skilledamateur-palindrome");
 
-function palindromeTester() {
-    let string = prompt("Please enter a string for palindrome testing:");
-    let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
     if (phrase.palindrome()) {
-        alert(`"${phrase.content}" is a palindrome!`);
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
     } else {
-        alert(`"${phrase.content}" is not a palindrome.`)
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is not a palindrome.`
     }
-}
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-    let button = document.querySelector("#palindromeTester");
-
-    button.addEventListener("click", function() {
-        palindromeTester();
-    });
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    })
 })
-},{"mhartl-palindrome":2}],2:[function(require,module,exports){
+},{"skilledamateur-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
 
-// Adds `reverse` to all strings.
-String.prototype.reverse = function() {
-  return Array.from(this).reverse().join("");
+// Adds 'reverse' to all strings.
+String.prototype.reverse = function () {
+    return Array.from(this).reverse().join("");
 }
 
+// Defines a Phrase object.
 function Phrase(content) {
-  this.content = content;
+    this.content = content;
 
-  // Return content processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return (this.content.match(/[a-z]/gi) || []).join("").toLowerCase();
-  }
-
-  // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    if (this.processedContent()) {
-      return this.processedContent() === this.processedContent().reverse();
-    } else {
-      return false;
+    this.processor = function(string) {
+        return this.content.toLowerCase();
     }
-  }
+
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.letters().toLowerCase();
+    }
+    
+    // Returns the letters in the content.
+    this.letters = function letters() {
+        const lettersRegEx = /[a-z]/gi;
+        return (this.content.match(lettersRegEx) || []).join("");
+    }
+
+    // Returns true for a palindrome, false otherwise.
+    this.palindrome = function palindrome() {
+        return this.processedContent() === this.processedContent().reverse();
+    }
+
+    // Makes the phrase LOUDER.
+    this.louder = function() {
+        return this.content.toUpperCase();
+    }
+
 }
-
-
 },{}]},{},[1]);
--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-let Phrase = require("mhartl-palindrome");
+let Phrase = require("skilledamateur-palindrome");
 
-function palindromeTester() {
-    let string = prompt("Please enter a string for palindrome testing:");
-    let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
     if (phrase.palindrome()) {
-        alert(`"${phrase.content}" is a palindrome!`);
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
     } else {
-        alert(`"${phrase.content}" is not a palindrome.`)
+        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is not a palindrome.`
     }
-}
+};
 
 document.addEventListener("DOMContentLoaded", function() {
-    let button = document.querySelector("#palindromeTester");
-
-    button.addEventListener("click", function() {
-        palindromeTester();
-    });
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    })
 })
